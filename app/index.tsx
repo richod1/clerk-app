046e39cf91a7e7c2df55f089ee86c215ff1a1261
richod1 <frimpsup@gmail.com>
2024-04-14 01:19:29 +0000

    deploying on gstore

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,9 @@
     import React from 'react';
-    import { View, Text, Image, StyleSheet, TouchableOpacity, Linking } from 'react-native';
+    import { View, Text, StyleSheet, TouchableOpacity, Linking,Image} from 'react-native';
     import Colors from '@/constants/Colors';
     import { Link } from 'expo-router';
     import welcomeImage from '@/assets/images/welcome.png';
     const welcome_image = Image.resolveAssetSource(welcomeImage).uri;
-
     const WelcomeScreen = () => {
     const openLink = () => {
         Linking.openURL('');  //here drop website link
